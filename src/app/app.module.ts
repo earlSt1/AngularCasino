f96a4d54a88766ffb86c2f42743a8bad7034cd3f
Craig Earl <craig@earlonics.com>
2019-09-08 20:20:48 +0100

    Added routing to application Home page with navbar added Styling improved slightly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,47 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { BlackjackComponent } from './blackjack/blackjack.component';
+import { HomeComponent } from './home/home.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'blackjack',
+    component: BlackjackComponent,
+    data: { title: 'AngularCasino - Blackjack' }
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: {title: 'AngularCasino - Home'}
+  },
+  { path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+    {
+        path: '**',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    }
+];
 
 @NgModule({
+  
   declarations: [
     AppComponent,
-    BlackjackComponent
+    BlackjackComponent,
+    HomeComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      //{ enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent,BlackjackComponent]
+  bootstrap: [AppComponent,BlackjackComponent,HomeComponent]
 })
 export class AppModule { }
