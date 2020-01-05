c7e91aff48bf4ebf9b037137472bf8b6c5428df3
Craig <craig@earlonics.com>
2020-01-05 14:50:05 +0000

    - Cleaning up imports - Storing jwt.secret in application.properties - Added HelloWorld controller to test auth

--- springboot/src/main/java/com/SecurityApplication.java
+++ springboot/src/main/java/com/SecurityApplication.java
@@ -12,18 +12,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class SecurityApplication {
---
+++ springboot/src/main/java/com/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
